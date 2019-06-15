06fa45baa9da9c9243a1320de2848cc2a169e4fc
Ray Kim <msca8h@naver.com>
2019-06-15 21:47:20 +0900

    initially implemented binary op code gen

--- code.c
+++ code.c
@@ -1,6 +1,8 @@
-#include "code.h"
 
 #include <stdarg.h>
+#include "code.h"
+
+#define GLOBAL_SCOPE  0
 
 void emitComment(FILE *out, const char *msg, ...)
 {
@@ -13,4 +15,74 @@ void emitComment(FILE *out, const char *msg, ...)
     va_end(args);
 
     fprintf(out, "\n");
+}
+
+void register_name(enum Storage reg, int reg_num, char* reg_name)
+{
+    char reg_prefix ;
+    switch(reg)
+    {
+    case Temp:
+        reg_prefix = 't';
+        break;
+    case SavedTemp:
+        reg_prefix = 's';
+        break;
+    case Return:
+        reg_prefix = 'v';
+        break;
+    case Argument:
+        reg_prefix = 'a';
+        break;
+    default:
+        reg_prefix = '0';
+        break;
+    }
+    sprintf(reg_name, "%c%d", reg_prefix, reg_num);
+}
+
+void load_local(Node t, enum Storage reg, int reg_num)
+{
+    char reg_name[2];
+    register_name(reg, reg_num, reg_name);
+
+    if(t->record->scope == GLOBAL_SCOPE)
+    {
+        /* incomplete */
+        fprintf(stdout, "la $%s, %s", "", "");
+    }
+    else
+    {
+        int loc = t->record->loc;
+        switch(t->storage) 
+        {
+        case Memory:
+            fprintf(stdout, "lw $%s, %d($fp)\n", reg_name, loc);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void exec_binop(Node t,
+                enum Storage rst_reg, int rst_reg_num,
+                enum Storage opa_reg, int opa_reg_num,
+                enum Storage opb_reg, int opb_reg_num)
+{
+    char opa_reg_name[3];
+    char opb_reg_name[3];
+    char rst_reg_name[3];
+    register_name(opa_reg, opa_reg_num, opa_reg_name);
+    register_name(opb_reg, opb_reg_num, opb_reg_name);
+    register_name(rst_reg, rst_reg_num, rst_reg_name);
+
+    switch(t->value.op)
+    {
+    case PLUS:
+        fprintf(stdout,"addu $%s, $%s, $%s\n", rst_reg_name, opa_reg_name, opb_reg_name);
+        break;
+    default:
+        break;
+    }
 }
